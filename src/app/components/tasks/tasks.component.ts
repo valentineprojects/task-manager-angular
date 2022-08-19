3c321c35ef2e3479275414a9fb91282057bcda98
Valentinas <scenass@gmail.com>
2022-08-19 23:22:04 +0300

    FIX: tsconfig absolute routing

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,13 @@
+import { TASKS } from 'app/mock';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-tasks',
+	templateUrl: './tasks.component.html',
+	styleUrls: ['./tasks.component.scss'],
+})
+export class TasksComponent implements OnInit {
+	constructor() {}
+
+	ngOnInit(): void {}
+}
---
+++ src/app/mock.ts
@@ -0,0 +1,22 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+	{
+		id: 1,
+		text: 'Learning angular',
+		day: 'August 25th at 2:30pm',
+		reminder: true,
+	},
+	{
+		id: 2,
+		text: 'Learning react',
+		day: 'August 26th at 1:30pm',
+		reminder: true,
+	},
+	{
+		id: 3,
+		text: 'Groceries',
+		day: 'August 27th at 2:30pm',
+		reminder: false,
+	},
+];
