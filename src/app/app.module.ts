c375d879183ab489504934091e396cafb0e8d685
Valentinas <scenass@gmail.com>
2022-08-21 19:50:18 +0300

    FEAT: Router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -11,6 +12,8 @@ import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
 import { AddTaskComponent } from './components/add-task/add-task.component';
+
+const appRoutes: Routes = [{ path: '', component: TasksComponent }];
 
 @NgModule({
 	declarations: [
@@ -27,6 +30,7 @@ import { AddTaskComponent } from './components/add-task/add-task.component';
 		FontAwesomeModule,
 		HttpClientModule,
 		FormsModule,
+		RouterModule.forRoot(appRoutes),
 	],
 	providers: [],
 	bootstrap: [AppComponent],
